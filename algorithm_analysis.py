ac5b4fe812d5c2dcb3d4cb5da96990770b8d454a
Hongfei Yan <yanhf@pku.edu.cn>
2021-12-07 00:20:41 +0800

    add algorithm_analysis.py

---
+++ algorithm_analysis.py
@@ -0,0 +1,125 @@
+"""
+algorithm_analysis.py
+
+Description:
+
+A utility program to plot algorithmic time complexity of a function.
+
+Author: Mahesh Venkitachalam
+    modified by Hongfei Yan
+
+Website: electronut.in
+http://electronut.in/plotting-algorithmic-time-complexity-of-a-function-using-python/
+"""
+ 
+import matplotlib.pyplot as plt
+import numpy as np
+import timeit
+import math
+from functools import partial
+import random
+ 
+
+def Constant(N):
+    """
+    O(1) function
+    """
+    x = 1
+
+def Logarithmic(N):
+    x = [i for i in range( int(math.log2(N)) )]
+ 
+def Linear(N):
+    """
+    O(n) function
+    """
+    x = [i for i in range(N)]
+
+def N_Log_N(N):
+	x = [i for i in range(N* int(math.log2(N)))]
+    #for i in range(N)
+    #    for i in range( int(math.log2(N)) )
+    #        x = 1
+ 
+def Quadratic(N):
+	"""
+	O(n^2) function
+	"""
+	x = [i for i in range(int(math.pow(N, 2)))]
+    #for i in range(N):
+	#	for j in range(N):
+	#		x = 1
+ 
+def Cubic(N):
+	"""
+	O(n^3) function
+	"""
+	x = [i for i in range(int(math.pow(N, 3)))]
+    
+	#for i in range(N):
+	#	for j in range(N):
+	#		for k in range(N):
+	#			x = 1
+
+
+def Exponential(N):
+	x = [i for i in range(int(math.pow(2, N)))]
+
+def fshuffle(N):
+    # O(N)
+    random.shuffle(list(range(N)))
+ 
+def fsort(N):
+    x = list(range(N))
+    random.shuffle(x)
+    x.sort()
+ 
+def plotTC(fn, nMin, nMax, nInc, nTests):
+    """
+    Run timer and plot time complexity
+    """
+    x = []
+    y = []
+    for i in range(nMin, nMax, nInc):
+        N = i
+        testNTimer = timeit.Timer(partial(fn, N))
+        t = testNTimer.timeit(number=nTests)
+        x.append(i)
+        y.append(math.log2(t))
+
+    #plt.plot(x, y, 'o', label=fn.__name__)
+    plt.plot(x, y, label=fn.__name__)
+    #plt.scatter(x, y)
+    #plt.text(x, y, fn.__name__)
+
+    #plt.scatter(x, y, label=fn.__name__)
+    plt.legend(loc='best')
+ 
+
+def main():
+    print('Analyzing Algorithms...')
+ 
+    upbound = 24
+    step = 1
+    seven_functions = [Constant,Logarithmic,Linear,N_Log_N,Quadratic,Cubic,Exponential]
+    for i in seven_functions:
+        plotTC(i, 10, upbound, step, 10)
+    
+    '''
+    plotTC(Constant, 10, upbound, step, 10)
+    plotTC(Logarithmic, 10, upbound, step, 10)
+    plotTC(Linear, 10, upbound, step, 10)
+    plotTC(N_Log_N, 10, upbound, step, 10)
+    plotTC(Quadratic, 10, upbound, step, 10)
+    plotTC(Cubic, 10, upbound, step, 10)
+    plotTC(Exponential, 10, upbound, step, 10)
+    '''
+    #plotTC(fshuffle, 10, 1000, 1000, 10)
+    #plotTC(fsort, 10, 1000, 10, 10)
+ 
+    plt.title("Seven Important Functions")
+    plt.show()
+ 
+# call main
+if __name__ == '__main__':
+    main()
